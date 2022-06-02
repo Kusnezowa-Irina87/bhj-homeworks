164ab4b77b891d11cbd324193031dcdd8de6e5eb
Irina Shtaup <irenmars@mail.ru>
2022-06-02 16:53:03 +0200

    Корректировка решения 3

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -18,18 +18,17 @@ const imageCookie = document.getElementById("cookie");
 const clickerCounter = document.getElementById("clicker__counter");
 const clickRate = document.getElementById("click__rate");
 
-let start = 0;
+let start = Date.now();
 
 imageCookie.onclick = function() {
     clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
+    // console.log("клики " + clickerCounter.textContent)
 
-    if (start === 0) {
-        start = Date.now();
-    } else {
-       let end = Date.now();
-       let elapsed = (end - start)/1000;
-       clickRate.textContent = (clickerCounter.textContent/elapsed).toFixed(2);
-    }
+    let end = Date.now();
+    let elapsed = (end - start)/1000;
+    // console.log("секунды " + elapsed);
+    clickRate.textContent = (clickerCounter.textContent/elapsed).toFixed(2);
+    // console.log("кликов в сек " + clickRate.textContent);
 
     if (imageCookie.width === 200) {
         imageCookie.width = 150;
